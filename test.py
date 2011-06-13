e6045ba85bba97795b93e675fc1c2111432e1994
Godefroid Chapelle <gotcha@bubblenet.be>
2011-06-13 17:19:30 +0200

    use timeouts correctly
    better output

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 import pty
 
 
-def readToTerminal(term, fd):
+def readToTerminal(term, fd, timeout=0.1):
     import os
     import select
-    readable, writable, errp = select.select([fd], [fd], [])
+    readable = []
+    while not fd in readable:
+        readable, writable, errp = select.select([fd], [], [], 0.01)
     while fd in readable:
         result = os.read(fd, 8192)
         term.ProcessInput(result)
-        readable, writable, errp = select.select([fd], [fd], [])
+        readable, writable, errp = select.select([fd], [], [], timeout)
     rows = term.screen
     for row in rows:
         print row.tostring()
-    print '-'*80
+    print '=' * 80
+
 
 def sendToVim(fd, command):
     import os
@@ -20,22 +23,23 @@ def sendToVim(fd, command):
 #    while fd not in writable:
 #        readable, writable, errp = select.select([fd], [fd], [])
     print repr(command)
-    print '-'*80
+    print '-' * 80
     os.write(fd, command)
-    time.sleep(.2)
 
 pid, fd = pty.fork()
 if pid == 0:
-    pty.spawn('vim')
+    pty.spawn(['vim'])
 else:
-    import time
-    time.sleep(.2)
     from TermEmulator import V102Terminal
     term = V102Terminal(24, 80)
     readToTerminal(term, fd)
     sendToVim(fd, ':e bla.txt\n')
     readToTerminal(term, fd)
-    sendToVim(fd, 'oabcd')
+    sendToVim(fd, 'Goabcd')
     readToTerminal(term, fd)
-    sendToVim(fd, '\x1b:wq\n')
+    sendToVim(fd, '\x1b:w')
+    readToTerminal(term, fd)
+    sendToVim(fd, '\n')
+    readToTerminal(term, fd)
+    sendToVim(fd, ':q\n')
     readToTerminal(term, fd)
