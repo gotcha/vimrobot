288d39d00a527d03302882a7c09bccf02d929b0c
Godefroid Chapelle <gotcha@bubblenet.be>
2011-07-10 10:35:09 +0200

    use class; third step
    better names

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import pty
 
 
-class ControledVim(object):
+class Controller(object):
 
     def __init__(self, term, fd):
         self.term = term
         self.fd = fd
 
-    def readToTerminal(self, timeout=0.1):
+    def update(self, timeout=0.1):
         import os
         import select
 
@@ -17,37 +17,57 @@ class ControledVim(object):
             readable, writable, errp = select.select([fd], [], [], 0.01)
         while fd in readable:
             result = os.read(fd, 8192)
-            term.ProcessInput(result)
+            self.term.ProcessInput(result)
             readable, writable, errp = select.select([fd], [], [], timeout)
-        rows = self.term.screen
-        for row in rows:
-            print row.tostring()
-        print '=' * 80
 
-    def sendToVim(self, command):
+    @property
+    def rows(self):
+        return self.term.screen
+
+    def send_keys(self, command):
         import os
-        print repr(command)
-        print '-' * 80
         os.write(self.fd, command)
 
 
-pid, fd = pty.fork()
-if pid == 0:
-    pty.spawn(['vim', '-u', 'NONE', 'afile.py'])
-else:
-    from TermEmulator import V102Terminal
-    term = V102Terminal(24, 80)
-    vim = ControledVim(term, fd)
-    vim.readToTerminal()
-    #vim.sendToVim(':e afile.py\n')
-    #vim.readToTerminal()
-    vim.sendToVim(':set nocp\n')
-    vim.readToTerminal()
-    vim.sendToVim('Goabcd')
-    vim.readToTerminal()
-    vim.sendToVim('\x1b:w')
-    vim.readToTerminal()
-    vim.sendToVim('\n')
-    vim.readToTerminal()
-    vim.sendToVim(':q\n')
-    vim.readToTerminal()
+def make_vim():
+    pid, fd = pty.fork()
+    if pid == 0:
+        pty.spawn(['vim', '-u', 'NONE', 'afile.py'])
+    else:
+        from TermEmulator import V102Terminal
+        term = V102Terminal(24, 80)
+        vim = Controller(term, fd)
+    return vim
+
+
+def send_command(vim, command):
+    print repr(command)
+    print '-' * 80
+    vim.send_keys(command)
+
+
+def print_status(vim):
+    vim.update()
+    for row in vim.rows:
+        print row.tostring()
+    print '=' * 80
+
+
+def main():
+    vim = make_vim()
+    print_status(vim)
+    #send_command(vim, ':e afile.py\n')
+    #print_status(vim)
+    send_command(vim, ':set nocp\n')
+    print_status(vim)
+    send_command(vim, 'Goabcd')
+    print_status(vim)
+    send_command(vim, '\x1b:w')
+    print_status(vim)
+    send_command(vim, '\n')
+    print_status(vim)
+    send_command(vim, ':q\n')
+    print_status(vim)
+
+if __name__ == '__main__':
+    main()
